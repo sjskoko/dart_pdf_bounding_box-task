a6cd7c5cfdd9abf0afd18b0bcda9e4899f64141e
sjskoko <77188135+sjskoko@users.noreply.github.com>
2022-08-30 13:40:30 +0900

    220830 pdf2img 추가

--- temp.py
+++ temp.py
@@ -1,7 +1,14 @@
-import pickle
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+from pdf2image import convert_from_bytes, convert_from_path
 
-with open('sample_img_file\[삼성바이오로직스]사업보고서(2022.03.21)\[삼성바이오로직스]사업보고서(2022.03.21)_text.pickle', 'rb') as fr:
-    data = pickle.load(fr)
 
 
-data[4]
+images = convert_from_path('sample_pdf_file/[삼성바이오로직스]사업보고서(2022.03.21).pdf')
+
+
+images[0].save('01.jpg', 'JPEG')
+im.show
